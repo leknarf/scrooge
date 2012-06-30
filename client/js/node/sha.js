d7895099eff75ec72f0d03b92aeb837d56100524
Andrew Frankel <andrew@leknarf.net>
2012-06-30 18:53:31 -0400

    really fix sha

--- client/js/node/sha.js
+++ client/js/node/sha.js
@@ -1,6 +1,3 @@
-var domready = require('domready');
 var sha = require('sha1');
 
-domready(function() {
-  document.sha = sha;
-});
+window.sha = sha;
--- client/js/sha.js
+++ client/js/sha.js
@@ -364,63 +364,6 @@ process.binding = function (name) {
 })();
 });
 
-require.define("/node_modules/domready/package.json",function(require,module,exports,__dirname,__filename,process){module.exports = {"main":"./ready.js"}});
-
-require.define("/node_modules/domready/ready.js",function(require,module,exports,__dirname,__filename,process){/*!
-  * domready (c) Dustin Diaz 2012 - License MIT
-  */
-!function (name, definition) {
-  if (typeof module != 'undefined') module.exports = definition()
-  else if (typeof define == 'function' && typeof define.amd == 'object') define(definition)
-  else this[name] = definition()
-}('domready', function (ready) {
-
-  var fns = [], fn, f = false
-    , doc = document
-    , testEl = doc.documentElement
-    , hack = testEl.doScroll
-    , domContentLoaded = 'DOMContentLoaded'
-    , addEventListener = 'addEventListener'
-    , onreadystatechange = 'onreadystatechange'
-    , readyState = 'readyState'
-    , loaded = /^loade|c/.test(doc[readyState])
-
-  function flush(f) {
-    loaded = 1
-    while (f = fns.shift()) f()
-  }
-
-  doc[addEventListener] && doc[addEventListener](domContentLoaded, fn = function () {
-    doc.removeEventListener(domContentLoaded, fn, f)
-    flush()
-  }, f)
-
-
-  hack && doc.attachEvent(onreadystatechange, fn = function () {
-    if (/^c/.test(doc[readyState])) {
-      doc.detachEvent(onreadystatechange, fn)
-      flush()
-    }
-  })
-
-  return (ready = hack ?
-    function (fn) {
-      self != top ?
-        loaded ? fn() : fns.push(fn) :
-        function () {
-          try {
-            testEl.doScroll('left')
-          } catch (e) {
-            return setTimeout(function() { ready(fn) }, 50)
-          }
-          fn()
-        }()
-    } :
-    function (fn) {
-      loaded ? fn() : fns.push(fn)
-    })
-})});
-
 require.define("/node_modules/sha1/package.json",function(require,module,exports,__dirname,__filename,process){module.exports = {"main":"sha1.js"}});
 
 require.define("/node_modules/sha1/sha1.js",function(require,module,exports,__dirname,__filename,process){(function() {
@@ -639,12 +582,9 @@ require.define("/node_modules/sha1/node_modules/charenc/charenc.js",function(req
 module.exports = charenc;
 });
 
-require.define("/js/node/sha.js",function(require,module,exports,__dirname,__filename,process){var domready = require('domready');
-var sha = require('sha1');
+require.define("/js/node/sha.js",function(require,module,exports,__dirname,__filename,process){var sha = require('sha1');
 
-domready(function() {
-  document.sha = sha;
-});
+window.sha = sha;
 });
 require("/js/node/sha.js");
 })();
